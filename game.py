a227030f796950d4ee98f20be9a3af0e1d762b39
Don E. Llopis <llopis.don@gmail.com>
2010-06-22 23:23:50 -0400

    Fixed email address in game.py.

--- game.py
+++ game.py
@@ -4,7 +4,7 @@
 Mushroom_and_Monsters -- a python+pygame based remake of the Atari
 arcade version of Millipede.
 
-Copyright 2007 Donald E. Llopis (thetofucube@gmail.com)
+Copyright 2007 Donald E. Llopis (machinezilla@gmail.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
